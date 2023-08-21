98fdfea326bde1c2e82965ae6f8869d6f954ce70
Achmad <sssnerv@gmail.com>
2023-08-21 18:46:27 +0700

    flow prototype image, revisi 2

--- ppdb_smp01bantarkawung/application/views/web/step/1.php
+++ ppdb_smp01bantarkawung/application/views/web/step/1.php
@@ -11,7 +11,7 @@ $user = $this->db->get('tbl_user')->row_array();
   </div>
   <!-- <div class="panel-body"> -->
   <img src="img/kontak.png" width="100%">
-  <!-- <ol style="color:#275555ff; line-height: 10px;">
+  <ol style="color:#275555ff; line-height: 10px;">
       <li style="line-height: 15px;">Setiap calon peserta didik wajib mengisi form pendaftaran dengan lengkap. </li>
       <li style="line-height: 15px;">Data-data yang diisikan pada form PPDB Online harus sesuai dengan data asli dan benar adanya.</li>
       <li style="line-height: 15px;">Calon peserta didik yang sudah mendaftarkan secara online akan mendapatkan Nomor Pendaftaran yang harus dicetak dan dilampirkan dalam persyaratan yang diminta oleh Panitia PPDB Online <?php echo $user['nama_lengkap']; ?>. </li>
@@ -19,6 +19,6 @@ $user = $this->db->get('tbl_user')->row_array();
       <li style="line-height: 15px;">Calon peserta didik yang sudah mendaftarakan diri melalui PPDB Online <?php echo $user['nama_lengkap']; ?> wajib menyerahkan dokumen persyaratan yang sudah ditentukan oleh Panitia PPDB Online.</li>
       <li style="line-height: 15px;">Setiap calon peserta didik yang mendaftar wajib melaksanakan daftar ulang yang diadakan dan di tentukan waktu nya oleh panitia PPDB <?php echo $user['nama_lengkap']; ?>.</li>
       <li style="line-height: 15px;">Data yang sudah diberikan oleh Panitia PPDB Online <?php echo $user['nama_lengkap']; ?> hanya digunakan untuk keperluan penerimaan peserta didik baru. <strong class="text-success">Data yang dikirimkan akan dijaga kerahasiaannya dan tidak akan dipublikasikan oleh Panitia PPDB Online</strong>.</li>
-    </ol> -->
+    </ol>
   <!-- </div> -->
 </div>
